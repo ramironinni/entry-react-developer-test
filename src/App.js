b31de7db473ad9dd904be3bb3c63898a263fc410
ramironinni <ramironinni@gmail.com>
2022-02-21 10:37:18 -0300

    Install graphql and Apollo and set initial config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+    render() {
+        return <div className="App"></div>;
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+});
+
+client
+    .query({
+        query: gql`
+            query {
+                category {
+                    products {
+                        id
+                    }
+                }
+            }
+        `,
+    })
+    .then((result) => console.log(result));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        {/* <ApolloProvider client={client}> */}
+        <App />
+        {/* </ApolloProvider> */}
+    </React.StrictMode>,
+    document.getElementById('root')
 );
