faa25be20b973f8627c090beeaf6a15d1298793d
ramironinni <ramironinni@gmail.com>
2022-03-01 15:38:44 -0300

    Update Logo to center it

--- src/components/Header/Logo/Logo.js
+++ src/components/Header/Logo/Logo.js
@@ -1,10 +1,11 @@
 import { Component } from 'react';
 import logo from '../../../assets/logo.svg';
+import styles from './Logo.module.css';
 
 class Logo extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.logo}>
                 <img src={logo} alt="site logo" />
             </div>
         );
