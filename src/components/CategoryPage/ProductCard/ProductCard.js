ee73c463eccd5402608e613b12f8cf5d28a66d7e
ramironinni <ramironinni@gmail.com>
2022-03-03 17:15:20 -0300

    Update ProductCard styles and OutOfStockStyles

--- src/components/CategoryPage/ProductCard/ProductCard.js
+++ src/components/CategoryPage/ProductCard/ProductCard.js
@@ -4,33 +4,40 @@ import cartGreenIcon from '../../../assets/cart-green-icon.svg';
 import OutOfStock from './OutOfStock';
 
 class ProductCard extends Component {
+    outOfStockTextStyles = this.props.stock === 0 ? styles.outOfStockText : '';
+    outOfStockArticle = this.props.stock === 0 ? styles.outOfStockArticle : '';
+
     render() {
         return (
-            <div className={styles.container}>
-                {this.props.stock === 0 && <OutOfStock />}
-                <article className={styles.article}>
-                    <div className={styles.imageContainer}>
-                        <div>
-                            <img
-                                src={cartGreenIcon}
-                                alt="cart icon"
-                                className={styles.cartGreenIcon}
-                            />
-                        </div>
+            <article className={`${styles.article} ${this.outOfStockArticle}`}>
+                <div className={styles.imageContainer}>
+                    {this.props.stock === 0 && <OutOfStock />}
+                    <div>
                         <img
-                            src="https://dummyimage.com/350/884bcf/8accae"
-                            alt="product"
-                            className={styles.image}
+                            src={cartGreenIcon}
+                            alt="cart icon"
+                            className={styles.cartGreenIcon}
                         />
                     </div>
-                    <div className={styles.descriptionContainer}>
-                        <p className={styles.descriptionName}>
-                            {this.props.name}
-                        </p>
-                        <p className={styles.descriptionPrice}>$00.00</p>
-                    </div>
-                </article>
-            </div>
+                    <img
+                        src="https://dummyimage.com/350/884bcf/8accae"
+                        alt="product"
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.descriptionContainer}>
+                    <p
+                        className={`${styles.descriptionName} ${this.outOfStockTextStyles}`}
+                    >
+                        {this.props.name}
+                    </p>
+                    <p
+                        className={`${styles.descriptionPrice} ${this.outOfStockTextStyles}`}
+                    >
+                        $00.00
+                    </p>
+                </div>
+            </article>
         );
     }
 }
