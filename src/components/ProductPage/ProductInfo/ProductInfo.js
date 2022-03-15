d44012a8d3939c543ff819980b6320ca742cfd68
ramironinni <ramironinni@gmail.com>
2022-03-15 14:29:13 -0300

    Fix hmtl in description in ProductPage

--- src/components/ProductPage/ProductInfo/ProductInfo.js
+++ src/components/ProductPage/ProductInfo/ProductInfo.js
@@ -1,15 +1,21 @@
 import { Component } from 'react';
 import styles from './ProductInfo.module.css';
-import SizeCard from './SizeCard/SizeCard';
+import DOMPurify from 'dompurify';
 
 class ProductInfo extends Component {
+    dirty = this.props.description;
+
+    getHTMLDescription() {
+        return { __html: DOMPurify.sanitize(this.dirty) };
+    }
+
     render() {
         return (
             <div className={styles.productInfo}>
                 <p className={styles.productName}>{this.props.name}</p>
-                <p className={styles.productShortDesc}>
+                {/* <p className={styles.productShortDesc}>
                     {this.props.description}
-                </p>
+                </p> */}
 
                 {/* <SizeCard sizes={this.props.sizes} inputName={'ProductPage'} /> */}
                 <div className={styles.priceContainer}>
@@ -17,9 +23,13 @@ class ProductInfo extends Component {
                     <p className={styles.price}>$50.00</p>
                 </div>
                 <button className={styles.button}>Add to cart</button>
-                <p className={styles.productLongDesc}>
+                <div
+                    className={styles.productLongDesc}
+                    dangerouslySetInnerHTML={this.getHTMLDescription()}
+                ></div>
+                {/* <p className={styles.productLongDesc}>
                     {this.props.description}
-                </p>
+                </p> */}
             </div>
         );
     }
