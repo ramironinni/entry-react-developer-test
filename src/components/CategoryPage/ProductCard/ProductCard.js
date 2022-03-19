fbb55b93002625af0943b6acd9ee73723ea71527
ramironinni <ramironinni@gmail.com>
2022-03-19 16:43:18 -0300

    Update styles in ProductCard to use cart green icon as a button

--- src/components/CategoryPage/ProductCard/ProductCard.js
+++ src/components/CategoryPage/ProductCard/ProductCard.js
@@ -3,48 +3,70 @@ import styles from './ProductCard.module.css';
 import cartGreenIcon from '../../../assets/cart-green-icon.svg';
 import OutOfStock from './OutOfStock';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { cartActions } from '../../../store/cart-slice';
 
 class ProductCard extends Component {
     outOfStockTextStyles = !this.props.inStock ? styles.outOfStockText : '';
-    outOfStockArticle = !this.props.inStock ? styles.outOfStockArticle : '';
+    outOfStockProduct = !this.props.inStock ? styles.outOfStockProduct : '';
 
     render() {
         return (
-            <Link
-                className={`${styles.article} ${this.outOfStockArticle}`}
-                to={`/product/${this.props.id}`}
+            <div
+                className={`${styles.productCard} ${this.outOfStockProduct}`}
+                onClick={() => {
+                    this.props.add(this.props.id);
+                }}
             >
-                <div className={styles.imageContainer}>
-                    {!this.props.inStock && <OutOfStock />}
-                    <div>
-                        <img
-                            className={styles.cartGreenIcon}
-                            src={cartGreenIcon}
-                            alt="cart icon"
-                        />
-                    </div>
+                <div>
                     <img
-                        className={styles.image}
-                        src={this.props.image}
-                        alt="product"
+                        className={styles.cartGreenIcon}
+                        src={cartGreenIcon}
+                        alt="cart icon"
                     />
                 </div>
-                <div className={styles.descriptionContainer}>
-                    <p
-                        className={`${styles.descriptionName} ${this.outOfStockTextStyles}`}
-                    >
-                        {this.props.name}
-                    </p>
-                    <p
-                        className={`${styles.descriptionPrice} ${this.outOfStockTextStyles}`}
-                    >
-                        {this.props.priceSymbol}
-                        {this.props.priceAmount}
-                    </p>
-                </div>
-            </Link>
+                <Link
+                    className={styles.article}
+                    to={`/product/${this.props.id}`}
+                >
+                    <div className={styles.imageContainer}>
+                        {!this.props.inStock && <OutOfStock />}
+                        <img
+                            className={styles.image}
+                            src={this.props.image}
+                            alt="product"
+                        />
+                    </div>
+                    <div className={styles.descriptionContainer}>
+                        <p
+                            className={`${styles.descriptionName} ${this.outOfStockTextStyles}`}
+                        >
+                            {this.props.name}
+                        </p>
+                        <p
+                            className={`${styles.descriptionPrice} ${this.outOfStockTextStyles}`}
+                        >
+                            {this.props.priceSymbol}
+                            {this.props.priceAmount}
+                        </p>
+                    </div>
+                </Link>
+            </div>
         );
     }
 }
 
-export default ProductCard;
+const mapStateToProps = (state) => {
+    return {
+        cart: state.cart.cart,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        add: (id) => dispatch(cartActions.add({ id })),
+        // remove: (id) => dispatch(cartActions.remove({ id })),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCard);
